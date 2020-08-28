ca99d88a37798effe0ce795277a513eaef945bc6
Deepak jha <ddjha171@gmail.com>
2020-08-28 17:14:25 +0530

    Static temp data created

---
+++ src/data/Data.js
@@ -0,0 +1,77 @@
+export const detailList = [`prepTime`, `exp`, `skill`, `cousine`, `type`];
+export const iconsByTypes = {
+  prepTime: "fine",
+  exp: "badge",
+  skill: "energy",
+  cousine: "chemistry",
+  type: "drop"
+}
+
+export default [
+  {
+    id:1,
+    image:require("../../assets/pizza.png"),
+    title:"Pizza",
+    description:"Good Pizza",
+    prepTime:"1 hour",
+    exp:10,
+    skill:"easy",
+    cousine:"Vegan",
+    type:"Dehydrates"
+  },
+  {
+    id:2,
+    image:require("../../assets/pizza.png"),
+    title:"Pizza2",
+    description:"Good Pizza2",
+    prepTime:"1 hour2",
+    exp:10,
+    skill:"easy2",
+    cousine:"Vegan2",
+    type:"Dehydrates2"
+  },
+  {
+    id:3,
+    image:require("../../assets/pizza.png"),
+    title:"Pizza3",
+    description:"Good Pizza3",
+    prepTime:"1 hour3",
+    exp:10,
+    skill:"easy3",
+    cousine:"Vegan3",
+    type:"Dehydrates3"
+  },
+  {
+    id:4,
+    image:require("../../assets/pizza.png"),
+    title:"Pizza",
+    description:"Good Pizza",
+    prepTime:"1 hour",
+    exp:10,
+    skill:"easy",
+    cousine:"Vegan",
+    type:"Dehydrates"
+  },
+  {
+    id:5,
+    image:require("../../assets/pizza.png"),
+    title:"Pizza",
+    description:"Good Pizza",
+    prepTime:"1 hour",
+    exp:10,
+    skill:"easy",
+    cousine:"Vegan",
+    type:"Dehydrates"
+  },
+  {
+    id:6,
+    image:require("../../assets/pizza.png"),
+    title:"Pizza",
+    description:"Good Pizza",
+    prepTime:"1 hour",
+    exp:10,
+    skill:"easy",
+    cousine:"Vegan",
+    type:"Dehydrates"
+  },
+]
